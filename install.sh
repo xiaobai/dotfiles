a03f1862288bba6e25f346b61a52e583558da5b4
Alexander Langford <nirvashtzero@gmail.com>
2015-10-02 00:27:31 -0400

    Adding OS X compatibility changes

--- install.sh
+++ install.sh
@@ -5,18 +5,14 @@
 DOTFILES_DIR="$(pwd)"
 
 # shell/
+ln -svf $DOTFILES_DIR/shell/alias ~/.alias
 
 # shell/bash
 ln -svf $DOTFILES_DIR/shell/bash/bashrc ~/.bashrc
 ln -svf $DOTFILES_DIR/shell/bash/bash_profile ~/.bash_profile
-ln -svf $DOTFILES_DIR/shell/bash/alias ~/.alias
 
 # shell/zsh
 ln -svf $DOTFILES_DIR/shell/zsh/zshrc ~/.zshrc
-
-# urxvt/
-
-ln -svf $DOTFILES_DIR/urxvt/Xresources ~/.Xresources
 
 # colors/
 
@@ -32,14 +28,31 @@ ln -svf $DOTFILES_DIR/vim/vimrc ~/.vimrc
 ln -svfn $DOTFILES_DIR/vim/ftplugin ~/.vim/
 ln -svfn $DOTFILES_DIR/vim/colors ~/.vim/
 
-# awesome/
+
+# GNU/Linux systems only!
 
 if [[ $(uname -s) == "Linux" ]]; then
 
+    # awesome/
     if [[ ! -d ~/.config/awesome ]]; then
         mkdir -p ~/.config
     fi
 
     ln -svfn $DOTFILES_DIR/awesome/rc.lua ~/.config/awesome/rc.lua
+
+    # urxvt/
+
+    ln -svf $DOTFILES_DIR/urxvt/Xresources ~/.Xresources
     ln -svfn $DOTFILES_DIR/awesome/themes ~/.config/awesome/
+fi
+
+# OS X Systems only!
+if [[ $(uname -s) == "Darwin" ]]; then
+
+    if [[ ! -d ~/.iterm ]]; then
+        mkdir ~/.iterm
+    fi
+
+    ln -svfn $DOTFILES_DIR/iterm2 ~/.iterm
+
 fi
