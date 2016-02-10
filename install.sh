54032a7d2fcfb6b7b36d04e629bb002c6642f17c
Alexander Langford <nirvashtzero@gmail.com>
2016-02-09 21:36:45 -0500

    Changing color theme slightly

--- install.sh
+++ install.sh
@@ -18,10 +18,6 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-# colors/
-
-ln -svfn $DOTFILES_DIR/colors ~/.bashcolors
-
 # vim/
 
 if [[ ! -d ~/.vim ]]; then
