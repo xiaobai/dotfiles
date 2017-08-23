a5b53cb4a58e7efa37d1a13c1278ee34d203579a
Alex Langford <nirvashtzero@gmail.com>
2017-08-22 23:53:40 -0700

    Fix spacing on install.sh

--- install.sh
+++ install.sh
@@ -18,24 +18,19 @@ ln -svf $DOTFILES_DIR/shell/bash_profile ~/.bash_profile
 ln -svf $DOTFILES_DIR/shell/zshrc ~/.zshrc
 
 # tmux/
-
 ln -svf $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
 
 # vim/
-
 if [[ ! -d ~/.vim ]]; then
     mkdir ~/.vim
 fi
-
 ln -svf $DOTFILES_DIR/vim/vimrc ~/.vimrc
 ln -svfn $DOTFILES_DIR/vim/colors ~/.vim/
 
 # GNU/Linux systems only!
-
 if [[ $(uname -s) == "Linux" ]]; then
 
     # i3/
-
     if [[ ! -d ~/.config/i3 ]]; then
         mkdir -p ~/.config/i3
     fi
@@ -49,6 +44,5 @@ if [[ $(uname -s) == "Linux" ]]; then
 
 
     # urxvt/
-
     ln -svf $DOTFILES_DIR/urxvt/Xresources ~/.Xresources
 fi
